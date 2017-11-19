8dbdad0ab563ba36cb6e240f007ae7ab8e4e78ca
Yanping Xie <rea.s.xyp@gmail.com>
2017-11-19 19:17:17 +0100

    namelist of zip

--- code/read_mag_zip.py
+++ code/read_mag_zip.py
@@ -3,6 +3,7 @@ from zipfile import ZipFile
 dir_path = "/mnt/ds3lab/yanping/mag"
 #line_count = 0
 with ZipFile(dir_path+"/data/mag_papers_0.zip", "r") as fin:
+	print(fin.namelist())
 	while True:
 		for i in range(10):
 			line = fin.readline()
