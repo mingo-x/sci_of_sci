2816f262d3f10ed357992ebdbdd04e2ddadab840
Yanping <rea.s.xyp@gmail.com>
2017-11-19 19:11:23 +0100

    zip file reader

---
+++ code/read_mag_zip.py
@@ -0,0 +1,13 @@
+dir_path = "/mnt/ds3lab/yanping/mag"
+#line_count = 0
+with ZipFile(dir_path+"/data/mag_papers_0.zip", "r") as fin:
+	while True:
+    	for i in range(10):
+    		line = fin.readline()
+    		if not line:
+    			print("***end of file***")
+    			break
+    		print(line)
+    	cont = input()
+    	if cont != "":
+    		break
