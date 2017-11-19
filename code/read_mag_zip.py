4a266291e100308ec6e91809f6ffdb0af1060347
Yanping <rea.s.xyp@gmail.com>
2017-11-19 19:21:00 +0100

    read from files insize zip

--- code/read_mag_zip.py
+++ code/read_mag_zip.py
@@ -2,15 +2,18 @@ from zipfile import ZipFile
 
 dir_path = "/mnt/ds3lab/yanping/mag"
 #line_count = 0
-with ZipFile(dir_path+"/data/mag_papers_0.zip", "r") as fin:
-	print(fin.namelist())
-	while True:
-		for i in range(10):
-			line = fin.readline()
-			if not line:
-				print("***end of file***")
-				break
-			print(line)
-		cont = input()
-		if cont != "":
-			break
+with ZipFile(dir_path+"/data/mag_papers_0.zip", "r") as myzip:
+	zip_files = myzip.namelist()
+	for file_name in zip_files:
+		with myzip.open(file_name) as fin:
+			print("***reading",file_name,"***")
+			while True:
+				for i in range(15):
+					line = fin.readline()
+					if not line:
+						print("***end of",file_name,"***")
+						break
+					print(line)
+				cont = input()
+				if cont != "":
+					exit()
