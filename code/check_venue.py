9b91500ec2eb1e4f95ac9a567f9f367c68aa00b1
Yanping <rea.s.xyp@gmail.com>
2018-03-09 20:45:05 +0100

    check papers without venue

---
+++ code/check_venue.py
@@ -0,0 +1,36 @@
+from zipfile import ZipFile
+import json
+import time
+
+dir_path = "/mnt/ds3lab/yanping/mag"
+
+no_venue = 0
+no_fos = 0
+no_venue_and_fos = 0
+
+for idx in range(9):
+	with ZipFile(dir_path+"/data/mag_papers_"+str(idx)+".zip", "r") as myzip:
+		zip_files = myzip.namelist()
+		for file_name in zip_files:
+			print("zip",idx,file_name)
+			start_time = time.time()
+			a_count = 0
+			with myzip.open(file_name) as fin:
+				for line in fin:
+					a_count += 1
+					a = json.loads(line.decode('utf-8'))
+					if "venue" not in a:
+						no_venue += 1
+						if "fos" not in a:
+							no_fos += 1
+							no_venue_and_fos += 1
+					if "fos" not in a:
+						no_fos += 1
+					if a_count%100000==0:
+						print(a_count,time.time()-start_time, no_venue, no_fos, no_venue_and_fos)
+			end_time = time.time()
+			print(end_time-start_time)
+
+print("no_venue", no_venue)
+print("no_fos", no_fos)
+print("no_venue_and_fos", no_venue_and_fos)
---
+++ code/venue/get_labeled.py
@@ -0,0 +1,44 @@
+from zipfile import ZipFile
+import json
+import time
+
+dir_path = "/mnt/ds3lab/yanping/mag"
+
+out_labeled = dir_path+"/data/venue_labeled.json"
+out_unlabeled = dir_path+"/data/venue_unlabeled.json"
+
+for idx in range(9):
+	with ZipFile(dir_path+"/data/mag_papers_"+str(idx)+".zip", "r") as myzip:
+		zip_files = myzip.namelist()
+		for file_name in zip_files:
+			print("zip",idx,file_name)
+			start_time = time.time()
+			a_count = 0
+			with myzip.open(file_name) as fin:
+				for line in fin:
+					a_count += 1
+					a = json.loads(line.decode('utf-8'))
+					data = {}
+					data["id"] = a["id"]
+					if "venue" in a:
+						data["venue"] = a["venue"]
+					else:
+						print("WARNING: NO VENUE, id ", a["id"])
+					if a_count%100000==0:
+						print(a_count,len(venue_set),time.time()-start_time)
+			end_time = time.time()
+			print(end_time-start_time)
+
+print("start writing out")
+
+fout = open(out_path,"w")
+venue_count = 0
+start_time = time.time()
+for v in venue_set:
+	venue_count += 1
+	fout.write(v+"\n")
+	if venue_count%100000==0:
+		end_time = time.time()
+		print(venue_count,end_time-start_time)
+		start_time = time.time()
+fout.close()
