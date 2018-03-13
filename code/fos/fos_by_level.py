4f6fcf5dd9fc5ec8412411599497f9fa57847331
Yanping <rea.s.xyp@gmail.com>
2018-03-13 14:59:54 +0100

    change file suffix to pkl

--- code/fos/fos_by_level.py
+++ code/fos/fos_by_level.py
@@ -10,14 +10,14 @@ with open(hierarchy_file,"r") as fin:
 		level_set[int(edge[1][1])].add(edge[0])
 		level_set[int(edge[3][1])].add(edge[2])
 
-with open(dir_path+"/fos_l0.txt","wb") as fout:
+with open(dir_path+"/fos_l0.pkl","wb") as fout:
 	pickle.dump(level_set[0],fout,pickle.HIGHEST_PROTOCOL)
 
-with open(dir_path+"/fos_l1.txt","wb") as fout:
+with open(dir_path+"/fos_l1.pkl","wb") as fout:
 	pickle.dump(level_set[1],fout,pickle.HIGHEST_PROTOCOL)
 
-with open(dir_path+"/fos_l2.txt","wb") as fout:
+with open(dir_path+"/fos_l2.pkl","wb") as fout:
 	pickle.dump(level_set[2],fout,pickle.HIGHEST_PROTOCOL)
 
-with open(dir_path+"/fos_l3.txt","wb") as fout:
+with open(dir_path+"/fos_l3.pkl","wb") as fout:
 	pickle.dump(level_set[3],fout,pickle.HIGHEST_PROTOCOL)
