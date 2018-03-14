34443a1929dc2a1702992a5fa8e19cef7f3e63ac
Yanping <rea.s.xyp@gmail.com>
2018-03-14 16:26:21 +0100

    mapping l2/l3 to l0/l1

--- code/fos/cast.py
+++ code/fos/cast.py
@@ -1,5 +1,4 @@
 # cast l2/l3 fos to l1 fos
-# suppose no l0 for now
 import pickle
 
 dir_path = "/mnt/ds3lab/yanping/mag/data"
@@ -8,12 +7,12 @@ fos_level_path = dir_path+"/fos_level.pkl"
 fos_parent_path = dir_path+"/fos_parent.pkl"
 out_path = dir_path+"/venue_fos_l1.pkl"
 
-def get_l1_pa(fos,fos_pa,fos_level):
+def get_pa(fos,fos_pa,fos_level):
 	pa = fos
 	pa_level = fos_level[fos]
 	while pa_level>1:
 		if pa in fos_pa:
-			new_p = fos_pa[fos]
+			new_p = fos_pa[pa]
 			pa = new_p[0]
 			pa_level = new_p[1]
 		else:
@@ -30,17 +29,26 @@ if __name__ == "__main__":
 	with open(fos_parent_path,"rb") as fin:
 		fos_pa = pickle.load(fin)
 
+	counter_0 = 0
+	counter_1 = 1
+	mapping = {}
 	for v in venue_fos:
-		# assume one fos per venue
-		fos = venue_fos[v]
-		if fos_level[fos]>1:
-			l1_pa = get_l1_pa(fos,fos_pa,fos_level)
-			if l1_pa == fos:
-				print("WARNING: No L1 Parent",fos_level[fos])
-			venue_fos[v] = l1_pa
+		cast_set = set()
+		for fos in venue_fos[v]:
+			pa = fos
+			if fos_level[fos]>1:
+				pa = get_pa(fos,fos_pa,fos_level)
+			cast_set.add(pa)
+		if len(cast_set) == 0:
+			counter_0 += 1
+		elif len(cast_set) > 1:
+			counter_1 += 1
+			#print("WARNING: more than 1 FOS")
+		print("******",v,len(cast_set))
+		mapping[v] = cast_set
 
 	with open(out_path,"wb") as fout:
-		pickle.dump(venue_fos,fout,pickle.HIGHEST_PROTOCOL)
+		pickle.dump(mapping,fout,pickle.HIGHEST_PROTOCOL)
 
 
 
