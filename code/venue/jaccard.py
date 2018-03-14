1ce4ad42227b52e3a7eb6cab83007a053c0b2c21
Yanping <rea.s.xyp@gmail.com>
2018-03-14 16:37:32 +0100

    filter stopword

--- code/venue/jaccard.py
+++ code/venue/jaccard.py
@@ -5,6 +5,7 @@ import json
 import pickle
 from stemming.porter2 import stem
 from time import time
+from nltk.corpus import stopwords
 #from nltk.stem import PorterStemmer
 
 dir_path = "/mnt/ds3lab/yanping/mag/data"
@@ -36,23 +37,27 @@ def load_fos(path):
 	return dic
 
 # split string to set of words
-def split_and_stem(s):
+def split_and_stem(s,stopword):
 	words = s.lower().split(" ")
 	word_set = set()
 	
 	for w in words:
-		word_set.add(stem(w))
+		nw = stem(w)
+		if nw in stopword:
+			continue
+		word_set.add(nw)
 	return word_set
 
 
 if __name__ == "__main__":
 	#ps = PorterStemmer()
+	stopword = stopwords.words("english")
 	venues = load_venue(venue_path)
 	foss = load_fos(fos_path)
 	# turn fos string into set
 	fos_sets = {}
 	for key in foss:
-		fos_sets[key] = split_and_stem(foss[key])
+		fos_sets[key] = split_and_stem(foss[key],stopword)
 	mapping = {}
 	with open(fos_level_path,"rb") as fin:
 		fos_level = pickle.load(fin)
@@ -63,7 +68,7 @@ if __name__ == "__main__":
 		best_score = 0.0
 		best_hit = 0
 		best_fos = []
-		venue_set = split_and_stem(venue)
+		venue_set = split_and_stem(venue,stopword)
 		res = {}
 		# iterate over fos_sets
 		for fos in fos_sets:
