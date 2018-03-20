8409505da9791b2dee30e30d0b3fb3895f875c14
Yanping <rea.s.xyp@gmail.com>
2018-03-20 21:51:38 +0100

    count problematic cases

--- code/venue/jaccard.py
+++ code/venue/jaccard.py
@@ -47,13 +47,13 @@ def load_fos(path):
 	return dic
 
 # split string to set of words
-def split_and_stem(s,stopword,tknz,stemmer):
+def split_and_stem(s,stopword,tknz,stemmer,prep_set):
 	words = tknz.tokenize(s.lower())
 	word_set = set()
 	and_flag = False
 
 	for w in words:
-		if w=="and" or w=="&":
+		if w in prep_set:
 			and_flag = True
 		if w in stopword:
 			continue
@@ -75,10 +75,11 @@ if __name__ == "__main__":
 	tknz = TweetTokenizer()
 	venues = load_venue(venue_path)
 	foss = load_fos(fos_path)
+	prep_set = set(["of","and","&","in","on"])
 	# turn fos string into set
 	fos_sets = {}
 	for key in foss:
-		fos_sets[key], and_flag = split_and_stem(foss[key],stopword,tknz,stemmer)
+		fos_sets[key], and_flag = split_and_stem(foss[key],stopword,tknz,stemmer,prep_set)
 		if len(fos_sets[key])==0:
 			print(key)
 	mapping = {}
@@ -89,15 +90,20 @@ if __name__ == "__main__":
 	total = 0
 	start_time = time()
 	and_count = 0
+	dup_count = 0
+	many_count = 0
+	prob_count = 0
 	for venue in venues:
+		prob_flag = False
 		best_score = 0.0
 		best_hit = 0
 		best_fos = []
-		venue_set, and_flag = split_and_stem(venue,stopword,tknz,stemmer)
+		venue_set, and_flag = split_and_stem(venue,stopword,tknz,stemmer,prep_set)
 		if len(venue_set)==0:
 			print(venue)
 		if and_flag:
 			and_count += 1
+			prob_flag = True
 		res = {}
 		# iterate over fos_sets
 		for fos in fos_sets:
@@ -110,21 +116,39 @@ if __name__ == "__main__":
 				best_fos.append(fos)
 			elif score == best_score and score != 0:
 				best_fos.append(fos)
+		# check for duplicate
+		dup_set = set()
+		for fos in best_fos:
+			if foss[fos] in dup_set:
+				dup_count += 1
+				prob_flag = True
+				break
+			else:
+				dup_set.add(foss[fos])
+		# too many foss
+		if len(best_fos) > 4:
+			many_count += 1
+			prob_flag = True
 		mapping[venue] = best_fos
+		# zero fos
 		if len(best_fos) == 0:
 			counter_0 += 1
-		elif len(best_fos) > 1:
-			counter_1 += 1
+			prob_flag = True
+		if prob_flag:
+			prob_count += 1
 		print("---",venue)
 		for f in best_fos:
 			print("***",f,foss[f])
+
 		total += 1
 		if total == 1000:
 			break
 	print("0 FOS:",counter_0)
-	print("more than 1 FOS:",counter_1)
+	print("too many FOSs:",many_count)
+	print("duplicate FOSs:",dup_count)
 	print("time:",time()-start_time)
 	print("venue with \'and\':", and_count, "in total:", total)
+	print("problematic venues:",prob_count)
 	with open(out_path,"wb") as fout:
 		pickle.dump(mapping,fout,pickle.HIGHEST_PROTOCOL)
 
