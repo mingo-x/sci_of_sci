d8fbfbd1bc5b914d513d0c02c9ef27f0dd6f9358
Yanping <rea.s.xyp@gmail.com>
2018-04-22 21:02:06 +0200

    add venue-domain mapping. add id as index

--- code/db/mongo_db.py
+++ code/db/mongo_db.py
@@ -1,13 +1,20 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 from zipfile import ZipFile
 import json
 import time
+import pickle
 
 client = MongoClient()
 db = client.mag
 papers = db.papers
+papers.create_index([('id', ASCENDING)], unique=True)
 dir_path = "/mnt/ds3lab/yanping/mag/"
+venue_fos_mapping_path = dir_path+"data/venue_fos_l1.pkl"
 
+with open(venue_fos_mapping_path,"rb") as fin: # load in the venue-fos mapping
+	venue_fos_mapping = pickle.load(fin)
+
+bulk = []
 for idx in range(1):
 	with ZipFile(dir_path+"/data/mag_papers_"+str(idx)+".zip", "r") as myzip:
 		zip_files = myzip.namelist() # get the list of file names inside one zipped package
@@ -21,8 +28,16 @@ for idx in range(1):
 						continue
 					if "venue" not in a:
 						continue
-					_id = papers.insert_one(a)
-					# print(_id)
+					else:
+						venue = a["venue"]
+						a["domain"] = venue_fos_mapping[venue]
+						bulk.append(a)
+					
+					if len(bulk) == 10:
+						papers.insert_many(bulk)
+						bulk = []
 						
 			end_time = time.time()
 			print("time",end_time-start_time)
+
+papers.insert_many(bulk)
