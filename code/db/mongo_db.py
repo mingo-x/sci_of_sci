cb02a4df14b7bba84b018339bc9fbf158509c3ae
Yanping <rea.s.xyp@gmail.com>
2018-04-22 20:09:48 +0200

    import data to mongodb

---
+++ code/db/mongo_db.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+from zipfile import ZipFile
+import json
+import time
+import pickle
+
+client = MongoClient('mongodb://127.0.0.1:27017/',unicode_decode_error_handler='ignore')
+db = client.mag
+papers = db.papers
+dir_path = "/mnt/ds3lab/yanping/mag/"
+
+for idx in range(1):
+	with ZipFile(dir_path+"/data/mag_papers_"+str(idx)+".zip", "r") as myzip:
+		zip_files = myzip.namelist() # get the list of file names inside one zipped package
+		for file_name in zip_files:
+			print("zip",idx,file_name)
+			start_time = time.time()
+			with myzip.open(file_name) as fin:
+				for line in fin: # each line is a piece of json data
+					a = json.loads(line.decode('utf-8')) # decode json
+					if "lang" in a and a["lang"]!="en": # skip papers without tag "en"
+						continue
+					if "venue" not in a:
+						continue
+					_id = papers.insert_one(a)
+						
+			end_time = time.time()
+			print("time",end_time-start_time)
